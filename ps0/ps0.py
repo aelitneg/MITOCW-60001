3d04fcdf209afd39e473b62440c286b210719b51
Andrew Gentile <agentile1990@gmail.com>
2020-04-18 21:51:29 -0500

    Problem Set 0

---
+++ ps0/ps0.py
@@ -0,0 +1,19 @@
+import numpy
+
+input_x = input("Enter number x: ")
+x = float(input_x)
+
+input_y = input("Ener number y: ")
+y = float(input_y)
+
+print("x**y:", x**y)
+
+print("log(x)", numpy.log2(x))
+
+
+
+
+
+
+
+
